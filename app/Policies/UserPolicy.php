3d586625642d96b33151e5ba982c387fbf4743f8
Matilde Simões <up202108782@up.pt>
2023-11-18 22:56:26 +0000

    fix name error

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Support\Facades\Auth;
 
-class AdminPolicy{
+class UserPolicy{
 
     use HandlesAuthorization;
 

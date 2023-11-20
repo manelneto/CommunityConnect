9c46f67c54e8777c759ab4bb03068c8e7760a013
Manuel Neto <manuelrlcneto@gmail.com>
2023-11-20 15:08:59 +0000

    logout button redirects to questions

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login')
+        return redirect()->route('questions')
             ->withSuccess('You have logged out successfully!');
     } 
 }

dbf19a34c3bcd525e53e74fe22a37e372d642138
Matilde Simões <up202108782@up.pt>
2023-11-20 18:32:46 +0000

    added ask-question page css

--- resources/views/layouts/app-cc.blade.php
+++ resources/views/layouts/app-cc.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('css/error.css') }}" rel="stylesheet">
     <link href="{{ asset('css/edit-profile.css') }}" rel="stylesheet">
     <link href="{{ asset('css/profile-page.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/ask-question.css') }}" rel="stylesheet">
 
     <!-- JavaScript -->
     <script src="{{ asset('js/app.js') }}" defer></script>

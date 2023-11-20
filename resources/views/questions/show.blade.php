f1c09cbc08f8def53fd7f6524c595ed2e40124ac
antoniorama <antonio.rama.03@gmail.com>
2023-11-20 16:32:33 +0000

    leave an answer css

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -19,11 +19,19 @@
                 <!-- question -->
 
                 @include('partials.question', ['question' => $question])
-
-                <div class="leave-answer-container">
-                    <button class="leave-answer-button">Leave an Answer</button>
-                </div>
-
+                @auth
+                    <div class="leave-answer-container">
+                        <span class="leave-answer-text">Leave an Answer</span>
+                        <form action="/answers" method="POST">
+                            @csrf
+                            <textarea id="content" name="content" rows="6" cols="60"></textarea>
+                            <br>
+                            <input type="hidden" name="id_question" value="{{ $question->id }}">
+                            <input type="hidden" name="id_user" value="{{ Auth::user()->id }}">
+                            <input type="submit" value="Post Answer">
+                        </form>
+                    </div>
+                @endauth
                 <!-- answers -->
 
                 @foreach ($answers as $answer)
@@ -39,17 +47,6 @@
                         </ul>
                     </div>
                 @endif
-                @auth
-                    <form action="/answers" method="POST">
-                        @csrf
-                        <label for="content">Your Answer:</label>
-                        <textarea id="content" name="content" rows="4" cols="50"></textarea>
-                        <br>
-                        <input type="hidden" name="id_question" value="{{ $question->id }}">
-                        <input type="hidden" name="id_user" value="{{ Auth::user()->id }}">
-                        <input type="submit" value="Post Answer">
-                    </form>
-                @endauth
             </section>
             @yield('right')
         </div>

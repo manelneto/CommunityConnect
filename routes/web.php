315b09490afd318e81c3279ce523b353fd8eb9b1
Matilde Simões <up202108782@up.pt>
2023-12-01 17:36:09 +0000

    modified first page of the site

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Home
-Route::redirect('/', '/questions');
+Route::redirect('/', '/communities');
 
 // Authentication
 Route::controller(RegisterController::class)->group(function () {

d29a3af9e885f1c030011288d5166a5b717c66a8
antoniorama <antonio.rama.03@gmail.com>
2023-12-01 16:25:51 +0000

    about us content

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -3,6 +3,20 @@
 
 @section('main')
     <main id="about-us">
-        <div class="main-content"></div>
+        <div class="main-content">
+            <h1 class="about-us-title">About Us</h1>
+            <p> <strong>Community Connect</strong> is a web-based information system that allows users to share questions
+                and get answers on
+                various topics, with the aim of providing answers to common problems in an environment of mutual support.
+            </p>
+            <p>This project was developed for the <strong>LBAW (Laboratório de Bases de Dados e Aplicações Web)</strong>
+                curricular unit <strong>(3rd year L.EIC)</strong>, by:</p>
+            <ul>
+                <li>António Henrique Martins Azevedo, up202108689@up.pt</li>
+                <li>António Marujo Rama, up202108801@up.pt</li>
+                <li>Manuel Ramos Leite Carvalho Neto, up202108744@up.pt</li>
+                <li>Matilde Isabel da Silva Simões, up202108782@up.pt</li>
+            </ul>
+        </div>
     </main>
 @endsection

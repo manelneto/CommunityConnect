b5ba8c01efd14d498d050b9a7963ea112dba578b
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-03 23:49:15 +0000

    displaying comments

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,27 +2,55 @@
 @include ('layouts.errors')
 
 @section('main')
-    <main id="question-show">
-        <div class="main-content">
-            <section class="main-info">
-                @include('partials.question', ['question' => $question])
-                @auth
-                    <div class="leave-answer-container">
-                        <span class="leave-answer-text">Leave an Answer</span>
-                        <form action="/answers" method="POST">
+    <main id="question">
+        @yield('errors')
+        @include('partials.question', ['question' => $question])
+        <details class="comments">
+            <summary>Question Comments</summary>
+            @foreach ($question->comments as $comment)
+                @include('partials.question_comment', ['comment' => $comment])
+            @endforeach
+            <article class="leave">
+                <h2>Leave a Comment</h2>
+                <form action="/question-comments" method="post">
+                    @csrf
+                    <textarea name="content" rows="6" cols="60"></textarea>
+                    <input type="hidden" name="id_question" value="{{ $question->id }}">
+                    <button type="submit">Post Comment</button>
+                </form>
+            </article>
+        </details>
+        @auth
+            <article class="leave">
+                <h2 class="leave-answer-text">Leave an Answer</h2>
+                <form action="/answers" method="post">
+                    @csrf
+                    <textarea name="content" rows="6" cols="60"></textarea>
+                    <input type="hidden" name="id_question" value="{{ $question->id }}">
+                    <button type="submit">Post Answer</button>
+                </form>
+            </article>
+        @endauth
+        <section id="answers">
+            <h2> {{ $question->answers_count }} Answers</h2>
+            @foreach ($answers as $answer)
+                @include('partials.answer', ['answer' => $answer])
+                <details class="comments">
+                    <summary>Answer Comments</summary>
+                    @foreach($answer->comments as $comment)
+                        @include('partials.answer_comment', ['comment' => $comment])
+                    @endforeach
+                    <article class="leave">
+                        <h2>Leave a Comment</h2>
+                        <form action="/answer-comments" method="post">
                             @csrf
-                            <textarea id="content" name="content" rows="6" cols="60"></textarea>
-                            <input type="hidden" name="id_question" value="{{ $question->id }}">
-                            <input type="hidden" name="id_user" value="{{ Auth::user()->id }}">
-                            <input id="submit-answer" type="submit" value="Post Answer">
+                            <textarea name="content" rows="6" cols="60"></textarea>
+                            <input type="hidden" name="id_answer" value="{{ $answer->id }}">
+                            <button type="submit">Post Comment</button>
                         </form>
-                    </div>
-                @endauth
-                @foreach ($answers as $answer)
-                    @include('partials.answer', ['answer' => $answer])
-                @endforeach
-                @yield('errors')
-            </section>
-        </div>
+                    </article>
+                </details>
+            @endforeach
+        </section>
     </main>
 @endsection

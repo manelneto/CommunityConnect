4c37ef37d06d5ce724f79264c443f6992607410e
Matilde Simões <up202108782@up.pt>
2023-12-03 23:29:47 +0000

    added main-features users

--- resources/views/pages/main-features.blade.php
+++ resources/views/pages/main-features.blade.php
@@ -25,5 +25,28 @@
                 <li>Registration</li>
             </ul>
         </article> 
+        <article class="feature-section">
+            <h3>If you are an authenticated user, you can:</h3>
+            <ul> 
+                <li>View personal feed</li>
+                <li>Post questions and answers</li>
+                <li>View own questions and answers</li>
+                <li>Log out</li>
+                <li>View and edit profile</li>
+                <li>Like/dislike questions and answers</li>
+                <li>Comment on questions and answers</li>
+                <li>Mark interest in questions and tags</li>
+                <li>View and receive answer, comments, like/dislike and badge notifications</li>
+                <li>Recover password</li>
+                <li>Delete own account</li>
+                <li>Profile picture</li>
+                <li>View personal notifications</li>
+                <li>Receive badges</li>
+                <li>Become an Expert</li>
+                <li>Attach Files</li>
+                <li>Have a Rating</li>
+                <li>Follow a Community</li>
+            </ul> 
+        </article>
     </section>
 @endsection

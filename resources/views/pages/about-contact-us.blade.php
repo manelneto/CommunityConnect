b269ea31772cc94e781b4caea3bd23e42614f38d
Matilde Simões <up202108782@up.pt>
2023-12-04 16:25:45 +0000

    clean divs

--- resources/views/pages/about-contact-us.blade.php
+++ resources/views/pages/about-contact-us.blade.php
@@ -2,22 +2,20 @@
 
 @section('main')
     <main id="about-us">
-        <div class="main-content">
-          <section class="about-us">
-            <h1 class="about-us-title">About Us</h1>
-            <p class="about-cc-text-1"> <strong>Community Connect</strong> is a web-based information system that allows users to share questions
-                and get answers on
-                various topics, with the aim of providing answers to common problems in an environment of mutual support.
-            </p>
-            <p class="about-cc-text-2">This project was developed for the <strong>LBAW (Laboratório de Bases de Dados e Aplicações Web)</strong>
-                curricular unit <strong>(3rd year L.EIC)</strong>, by:</p>
-            <ul class="made-by-list">
-                <li>António Henrique Martins Azevedo, <a href="mailto:up202108689@up.pt">up202108689@up.pt</a></li>
-                <li>António Marujo Rama, <a href="mailto:up202108801@up.pt">up202108801@up.pt</a></li>
-                <li>Manuel Ramos Leite Carvalho Neto, <a href="mailto:up202108744@up.pt">up202108744@up.pt</a></li>
-                <li>Matilde Isabel da Silva Simões, <a href="mailto:up202108782@up.pt">up202108782@up.pt</a></li>
-            </ul>
-          </section>
-        </div>
+        <section class="about-us">
+        <h1 class="about-us-title">About Us</h1>
+        <p class="about-cc-text-1"> <strong>Community Connect</strong> is a web-based information system that allows users to share questions
+            and get answers on
+            various topics, with the aim of providing answers to common problems in an environment of mutual support.
+        </p>
+        <p class="about-cc-text-2">This project was developed for the <strong>LBAW (Laboratório de Bases de Dados e Aplicações Web)</strong>
+            curricular unit <strong>(3rd year L.EIC)</strong>, by:</p>
+        <ul class="made-by-list">
+            <li>António Henrique Martins Azevedo, <a href="mailto:up202108689@up.pt">up202108689@up.pt</a></li>
+            <li>António Marujo Rama, <a href="mailto:up202108801@up.pt">up202108801@up.pt</a></li>
+            <li>Manuel Ramos Leite Carvalho Neto, <a href="mailto:up202108744@up.pt">up202108744@up.pt</a></li>
+            <li>Matilde Isabel da Silva Simões, <a href="mailto:up202108782@up.pt">up202108782@up.pt</a></li>
+        </ul>
+        </section>
     </main>
 @endsection

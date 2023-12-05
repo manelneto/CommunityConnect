f7bc2843696dfa5d56c8586d5b1a908682e88cd8
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-05 16:38:41 +0000

    added upload file on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,35 +2,38 @@
 @include ('layouts.errors')
 
 @section('main')
-    <main class="sign-page-main">
-        <div class="sign-page-main-content">
-            <div class="sign-page-left-content">
-                <h1 class="title-text">Join the Community</h1>
-                <h3 class="subtitle-text">Sign Up to Community Connect to ask <br> questions, answer people's questions, and <br> connect with others. </h3>
-                <a class="go-to-sign-in" href="/login">Have an Account? Sign In</a>
+    <main class="sign-page-main-content">
+        <article class="sign-page-left-content">
+            <h1 class="title-text">Join the Community</h1>
+            <h3 class="subtitle-text">Sign Up to Community Connect to ask <br> questions, answer people's questions, and <br> connect with others. </h3>
+            <a class="go-to-sign-in" href="/login">Have an Account? Sign In</a>
+        </article>
+        @yield('errors')
+        <form class="sign-page-right-content" action="{{ route('register') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="username">Username *</label>
+                <input id="username"  type="text" name="username" required>
             </div>
-            @yield('errors')
-            <form class="sign-page-right-content" action="{{ route('register') }}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="username">Username *</label>
-                    <input type="text" id="username" name="username" required>
-                </div>
-                <div class="form-group">
-                    <label for="email">Email *</label>
-                    <input type="email" id="email" name="email" required>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password *</label>
-                    <input type="password" id="password" name="password" required>
-                </div>
-                <div class="form-group">
-                    <label for="password_confirmation">Confirm password *</label>
-                    <input type="password" id="password_confirmation" name="password_confirmation" required>
-                </div>
-                <small>Password must contain at least 8 characters</small>
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
+            <div class="form-group">
+                <label for="email">Email *</label>
+                <input id="email" type="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password *</label>
+                <input id="password" type="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="password_confirmation">Confirm Password *</label>
+                <input id="password_confirmation" type="password" name="password_confirmation" required>
+            </div>
+            <small>Password must contain at least 8 characters</small>
+            <div class="form-group">
+                <label for="photo">Profile Photo</label>
+                <input id="photo" type="file" name="file" required>
+                <input type="hidden" name="type" value="profile">
+            </div>
+            <button type="submit">Sign Up</button>
+        </form>
     </main>
 @endsection

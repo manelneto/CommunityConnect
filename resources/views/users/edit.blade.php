d157f2443a78e050939068b95270b13d781abfe1
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-05 16:52:29 +0000

    added upload file on edit profile

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,20 +5,23 @@
     <main>
         <div class="main-content">
             @yield('errors')
-            <form method="post" id="edit-profile">
+            <form method="post" id="edit-profile" enctype="multipart/form-data">
                 @csrf
                 <h1><input type="text" name="username" value="{{ $user->username }}"></h1>
                 <img class="member-pfp question-member-pfp" src="{{ asset('assets/profile-images/test-profile-image.jpeg') }}" alt="User's profile picture" />
-                <label id="email-label" for="email">Email: </label>
+                <label id="email-label" for="email">Email</label>
                 <input id="email" type="email" name="email" value="{{ $user->email }}">
+                <label id="photo-label" for="photo">Profile Photo</label>
+                <input id="photo" type="file" name="file" required>
+                <input type="hidden" name="type" value="profile">
                 <button id="edit-password" class="edit-password-button">Edit Password</button>
-                <label class="edit-password" for="current-password">Current Password:</label>
+                <label class="edit-password" for="current-password">Current Password</label>
                 <input class="edit-password" id="current-password" type="password" name="current-password">
-                <label class="edit-password" for="password">New Password:</label>
+                <label class="edit-password" for="password">New Password</label>
                 <input class="edit-password" id="password" type="password" name="password">
-                <label class="edit-password" for="password_confirmation">Confirm Password:</label>
+                <label class="edit-password" for="password_confirmation">Confirm Password</label>
                 <input class="edit-password" id="password_confirmation" type="password" name="password_confirmation">
-                <button formaction="../../users/{{ $user->id }}">Edit</button>
+                <button formaction="../../users/{{ $user->id }}">Save</button>
             </form>
         </div>
     </main>

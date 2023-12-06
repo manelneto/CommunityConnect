2e5bd60c6f31a6ea6179ce835b9aac4d8ddfd6bf
antoniorama <antonio.rama.03@gmail.com>
2023-12-06 14:01:46 +0000

    add tags to questions

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -5,7 +5,6 @@
     <main id="question-edit">
         <div class="main-content">
             <section class="main-info">
-                @yield('errors')
                 <span class="edit-question-text">Edit Question</span>
                 <form class="question-container question-edit-container" method="post">
                     @csrf
@@ -19,6 +18,19 @@
                             </div>
                             <h2 class="question-title"><input class="question-title-edit" type="text" name="title" required value="{{ $question->title }}"></h2>
                             <textarea class="question-description non-movable-textarea" name="content" rows="6" cols="56">{{ $question->content }}</textarea>
+                            <div class="edit-question-tags">
+                                @foreach ($question->tags as $tag) 
+                                    <li class="question-tag margin-on-tags">
+                                        {{ $tag->name }}
+                                    </li>
+                                @endforeach
+                                <input id="add-tag" class="form-control" type="text" name="add-tag">
+                                @error('tag')
+                                    <span class="error-message-tag-add">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>  
                         </div>
                     </div>
                     <div class="edit-buttons">

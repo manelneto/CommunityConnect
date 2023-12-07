29805f56cd4fffa95dea420685358843a86f48f0
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-07 21:23:19 +0000

    add file to new question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -6,18 +6,21 @@
         @yield('errors')
         <section id="create-question">
             <h1>Ask a Question</h1>
-            <form action="{{ route('questions') }}" method="post">
+            <form action="{{ route('questions') }}" method="post" enctype="multipart/form-data">
                 @csrf
-                    <label for="title">Title</label>
-                    <input id="title" class="form-control" type="text" name="title" required>
-                    <label for="community">Choose a community</label>
-                    <select id="community" class="form-control" name="id_community" required>
-                        @foreach ($communities as $community)
-                            <option value="{{ $community->id }}">{{ $community->name }}</option>
-                        @endforeach
-                    </select>
-                    <label for="content">Content</label>
-                    <textarea id="content" class="form-control" name="content"  placeholder="Describe your question" required></textarea>
+                <label for="title">Title</label>
+                <input id="title" class="form-control" type="text" name="title" required>
+                <label for="community">Choose a community</label>
+                <select id="community" class="form-control" name="id_community" required>
+                    @foreach ($communities as $community)
+                        <option value="{{ $community->id }}">{{ $community->name }}</option>
+                    @endforeach
+                </select>
+                <label for="content">Content</label>
+                <textarea id="content" class="form-control" name="content"  placeholder="Describe your question" required></textarea>
+                <label for="file">File</label>
+                <input id="file" type="file" name="file" accept="image/png,image/jpg,image/jpeg,application/doc,application/pdf,application/txt">
+                <input type="hidden" name="type" value="question">
                 <button type="submit">Submit</button>
             </form>
         </section>

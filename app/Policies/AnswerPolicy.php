8c8c2a44e5b0d837935ac780d9367593cd481968
antoniorama <antonio.rama.03@gmail.com>
2023-12-09 16:08:12 +0000

    moderator edit and delete answers

--- app/Policies/AnswerPolicy.php
+++ app/Policies/AnswerPolicy.php
@@ -19,26 +19,51 @@ class AnswerPolicy {
 
     public function edit(User $user, Answer $answer): bool
     {
-        return ($user->id === Auth::user()->id) && ($answer->id_user === $user->id || Auth::user()->administrator);
+        $isModeratorOfCommunity = in_array(
+            $answer->question->id_community,
+            Auth::user()->moderatorCommunities->pluck('id')->toArray()
+        );
+
+        return ($user->id === Auth::user()->id) && (($answer->id_user === $user->id || Auth::user()->administrator) || $isModeratorOfCommunity);
     }
 
     public function update(User $user, Answer $answer): bool
     {
-        return ($user->id === Auth::user()->id) && ($answer->id_user === $user->id || Auth::user()->administrator);
+        $isModeratorOfCommunity = in_array(
+            $answer->question->id_community,
+            Auth::user()->moderatorCommunities->pluck('id')->toArray()
+        );
+
+        return ($user->id === Auth::user()->id) && (($answer->id_user === $user->id || Auth::user()->administrator) || $isModeratorOfCommunity);
     }
 
     public function destroy(User $user, Answer $answer): bool
     {
-        return ($user->id === Auth::user()->id) && ($answer->id_user === $user->id || Auth::user()->administrator);
+        $isModeratorOfCommunity = in_array(
+            $answer->question->id_community,
+            Auth::user()->moderatorCommunities->pluck('id')->toArray()
+        );
+
+        return ($user->id === Auth::user()->id) && (($answer->id_user === $user->id || Auth::user()->administrator) || $isModeratorOfCommunity);
     }
 
     public function correct(User $user, Answer $answer): bool
     {
-        return ($user->id === Auth::user()->id) && ($answer->id_user === $user->id || Auth::user()->administrator);
+        $isModeratorOfCommunity = in_array(
+            $answer->question->id_community,
+            Auth::user()->moderatorCommunities->pluck('id')->toArray()
+        );
+
+        return ($user->id === Auth::user()->id) && (($answer->id_user === $user->id || Auth::user()->administrator) || $isModeratorOfCommunity);
     }
 
     public function incorrect(User $user, Answer $answer): bool
     {
-        return ($user->id === Auth::user()->id) && ($answer->id_user === $user->id || Auth::user()->administrator);
+        $isModeratorOfCommunity = in_array(
+            $answer->question->id_community,
+            Auth::user()->moderatorCommunities->pluck('id')->toArray()
+        );
+
+        return ($user->id === Auth::user()->id) && (($answer->id_user === $user->id || Auth::user()->administrator) || $isModeratorOfCommunity);
     }
 }

2d13f7de4bd64990e8abe8bf2fe98e6f34c4cb19
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-12 18:47:06 +0000

    answers button now redirects to answer section

--- resources/views/partials/question.blade.php
+++ resources/views/partials/question.blade.php
@@ -46,12 +46,12 @@
             <p class="file"><a href="{{ asset($question->file) }}" target="_blank">Download file here</a></p>
         @endif
         <div class="answers-details">
-            <button class="question-answer-btn">
+            <a href="{{ route('question', ['id' => $question->id]) . '#answers' }}" class="question-answer-btn">
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M22.8 4.8H20.4V15.6H4.8V18C4.8 18.66 5.34 19.2 6 19.2H19.2L24 24V6C24 5.34 23.46 4.8 22.8 4.8ZM18 12V1.2C18 0.54 17.46 0 16.8 0H1.2C0.54 0 0 0.54 0 1.2V18L4.8 13.2H16.8C17.46 13.2 18 12.66 18 12Z" fill="#abacb1" />
                 </svg>
                 {{ $question->answers_count }} Answers
-            </button>
+            </a>
             <span class="question-upvotes" data-id="{{ $question->id }}">
                 @if (((Request::route()->getName() === 'question' || Request::route()->getName() === 'profile') && array_search(Auth::user()?->id, array_column($question->likes()->get()->toArray(), 'id_user')) !== false) || (Request::route()->getName() !== 'question' && Request::route()->getName() !== 'profile' && array_search(Auth::user()?->id, array_column($question->likes, 'id_user')) !== false))
                     <svg class="voted" width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">

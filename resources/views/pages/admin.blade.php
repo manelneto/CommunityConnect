346621057592e70547293f03453d69f8624a9577
Matilde Simões <up202108782@up.pt>
2023-12-17 19:54:46 +0000

    fix some css

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -31,7 +31,19 @@
             <summary>Create a user</summary>
             <form action="../../users" method="post" class="form-admin">
                 @csrf
-                <label for="username">Username</label>
+                <section class="add-tooltip">
+                    <label for="block-user">Username</label>
+                    <div class="tooltip-icon">
+                        <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="20px" viewBox="0 0 416.979 416.979" xml:space="preserve">
+                            <path d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85 c81.369,81.47,213.378,81.551,294.849,0.181C437.293,274.636,437.375,142.626,356.004,61.156z M237.6,340.786 c0,3.217-2.607,5.822-5.822,5.822h-46.576c-3.215,0-5.822-2.605-5.822-5.822V167.885c0-3.217,2.607-5.822,5.822-5.822h46.576 c3.215,0,5.822,2.604,5.822,5.822V340.786z M208.49,137.901c-18.618,0-33.766-15.146-33.766-33.765 c0-18.617,15.147-33.766,33.766-33.766c18.619,0,33.766,15.148,33.766,33.766C242.256,122.755,227.107,137.901,208.49,137.901z" />
+                        </svg>
+                        <p class="tooltip-text">
+                            You can block a user by entering their username in the respective field.
+                            By blocking a user, you remove their capabilities to interact with the website in any way apart from viewing content.
+                            When you're done, click on the <b>Block</b> button.
+                        </p>
+                    </div>
+                </section>
                 <input type="text" id="username" name="username" placeholder="Enter username here" class="user-details-input">
                 <p class="username-error">Username is already taken</p>
                 <label for="email">Email</label>
@@ -43,18 +55,7 @@
                 <label for="password_confirmation">Confirm Password</label>
                 <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm password here" class="user-details-input">
                 <p class="password-confirmation-error">Passwords do not match</p>
-                <section class="add-tooltip">
-                    <button class="admin-button submit" type="submit">Create User</button>
-                    <div class="tooltip-icon" id="user-edit-profile">
-                        <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="20px" viewBox="0 0 416.979 416.979" xml:space="preserve">
-                            <path d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85 c81.369,81.47,213.378,81.551,294.849,0.181C437.293,274.636,437.375,142.626,356.004,61.156z M237.6,340.786 c0,3.217-2.607,5.822-5.822,5.822h-46.576c-3.215,0-5.822-2.605-5.822-5.822V167.885c0-3.217,2.607-5.822,5.822-5.822h46.576 c3.215,0,5.822,2.604,5.822,5.822V340.786z M208.49,137.901c-18.618,0-33.766-15.146-33.766-33.765 c0-18.617,15.147-33.766,33.766-33.766c18.619,0,33.766,15.148,33.766,33.766C242.256,122.755,227.107,137.901,208.49,137.901z" />
-                        </svg>
-                        <p class="tooltip-text">
-                            You can create a user by entering their user details in the respective fields.
-                            When you're done, click on the <b>Create User</b> button.
-                        </p>
-                    </div>
-                </section>
+                <button class="admin-button submit" type="submit">Create User</button>
             </form>
         </details>
         <details>

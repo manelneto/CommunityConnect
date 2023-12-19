eed4960b88353a89529d7673c676ca70868e9087
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-19 01:14:12 +0000

    reviewed notifications

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,123 +1,63 @@
 const pusher = new Pusher("96d60c26f4314399aa8b", {
     cluster: "eu",
     encrypted: true
-  });
-  
+});
 
 function getUserId() {
     const link = document.querySelector('.my-account-button');
     if (link) {
-      return link.href.split('/').pop();
+        return link.href.split('/').pop();
     }
 }
 
+function createNotification(title, notification) {
+    const article = document.createElement('article');
+    article.classList.add('notification');
+
+    const h3 = document.createElement('h3');
+    h3.innerHTML = title;
+
+    const a = document.createElement('a');
+    a.href = `/questions/${notification.id_question}`;
+    a.innerHTML = notification.message;
+
+    article.appendChild(h3);
+    article.appendChild(a);
+
+    const notifications = document.getElementById('notifications');
+    notifications.appendChild(article);
+
+    window.setTimeout(() => document.getElementById('notifications').firstElementChild.remove(), 15000);
+}
+
 const channel = pusher.subscribe('CommunityConnect');
-channel.bind('answer', function(notification) {
-  if (getUserId() === notification.id_user) {
-    const article = document.createElement('article');
-    article.classList.add('notification');
 
-    const h3 = document.createElement('h3');
-    h3.innerHTML = 'New answer';
-
-    const a = document.createElement('a');
-    a.href = `/questions/${notification.id_question}`;
-    a.innerHTML = notification.message;
-
-    article.appendChild(h3);
-    article.appendChild(a);
-
-    const notifications = document.getElementById('notifications');
-    notifications.appendChild(article);
-
-    window.setTimeout(() => document.getElementById('notifications').firstElementChild.remove(), 15000);
-  }
-});
-
-channel.bind('commentQuestion', function(notification) {
-  if (getUserId() === notification.id_user) {
-    const article = document.createElement('article');
-    article.classList.add('notification');
-
-    const h3 = document.createElement('h3');
-    h3.innerHTML = 'New comment on your question';
-
-    const a = document.createElement('a');
-    a.href = `/questions/${notification.id_question}`;
-    a.innerHTML = notification.message;
-
-    article.appendChild(h3);
-    article.appendChild(a);
-
-    const notifications = document.getElementById('notifications');
-    notifications.appendChild(article);
-
-    window.setTimeout(() => document.getElementById('notifications').firstElementChild.remove(), 15000);
-  }
-});
-
-channel.bind('commentAnswer', function(notification) {
+channel.bind('answer', function (notification) {
     if (getUserId() === notification.id_user) {
-        const article = document.createElement('article');
-        article.classList.add('notification');
+        createNotification('New answer', notification);
+    }
+});
 
-        const h3 = document.createElement('h3');
-        h3.innerHTML = 'New comment on your answer';
+channel.bind('commentQuestion', function (notification) {
+    if (getUserId() === notification.id_user) {
+        createNotification('New comment on your question', notification);
+    }
+});
 
-        const a = document.createElement('a');
-        a.href = `/questions/${notification.id_question}`;
-        a.innerHTML = notification.message;
-
-        article.appendChild(h3);
-        article.appendChild(a);
-
-        const notifications = document.getElementById('notifications');
-        notifications.appendChild(article);
-
-        window.setTimeout(() => document.getElementById('notifications').firstElementChild.remove(), 15000);
+channel.bind('commentAnswer', function (notification) {
+    if (getUserId() === notification.id_user) {
+        createNotification('New comment on your answer', notification);
     }
 });
 
 channel.bind('voteQuestion', function(notification) {
-  if (getUserId() === notification.id_user) {
-      const article = document.createElement('article');
-      article.classList.add('notification');
-
-      const h3 = document.createElement('h3');
-      h3.innerHTML = 'New vote on your question';
-
-      const a = document.createElement('a');
-      a.href = `/questions/${notification.id_question}`;
-      a.innerHTML = notification.message;
-
-      article.appendChild(h3);
-      article.appendChild(a);
-
-      const notifications = document.getElementById('notifications');
-      notifications.appendChild(article);
-
-      window.setTimeout(() => document.getElementById('notifications').firstElementChild.remove(), 15000);
-  }
+    if (getUserId() === notification.id_user) {
+        createNotification('New vote on your question', notification);
+    }
 });
 
 channel.bind('voteAnswer', function(notification) {
-  if (getUserId() === notification.id_user) {
-      const article = document.createElement('article');
-      article.classList.add('notification');
-
-      const h3 = document.createElement('h3');
-      h3.innerHTML = 'New vote on your answer';
-
-      const a = document.createElement('a');
-      a.href = `/questions/${notification.id_question}`;
-      a.innerHTML = notification.message;
-
-      article.appendChild(h3);
-      article.appendChild(a);
-
-      const notifications = document.getElementById('notifications');
-      notifications.appendChild(article);
-
-      window.setTimeout(() => document.getElementById('notifications').firstElementChild.remove(), 15000);
-  }
+    if (getUserId() === notification.id_user) {
+        createNotification('New vote on your answer', notification);
+    }
 });

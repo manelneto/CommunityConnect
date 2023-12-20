df2ece9358209b562b5f42d9e880f0eabc0fa330
Manuel Neto <manuelrlcneto@gmail.com>
2023-12-20 02:17:21 +0000

    reviewed show.blade.php

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::controller(QuestionController::class)->group(function () {
 
 // Question Comments
 Route::controller(QuestionCommentController::class)->group(function() {
-    Route::post('/question-comments', 'store');
+    Route::post('/question-comments', 'store')->name('question-comments');
     Route::post('/question-comments/{id}', 'update')->where('id', '[0-9]+')->name('edit-question-comment');
     Route::post('/question-comments/{id}/delete', 'destroy')->where('id', '[0-9]+')->name('delete-question-comment');
 });
@@ -76,14 +76,14 @@ Route::controller(QuestionCommentController::class)->group(function() {
 // Answers
 Route::controller(AnswerController::class)->group(function () {
     Route::get('/answers/{id}/edit', 'edit')->where('id', '[0-9]+')->name('edit-answer');
-    Route::post('/answers', 'store');
+    Route::post('/answers', 'store')->name('answers');
     Route::post('/answers/{id}', 'update')->where('id', '[0-9]+');
     Route::post('/answers/{id}/delete', 'destroy')->where('id', '[0-9]+')->name('delete-answer');
 });
 
 // Answer Comments
 Route::controller(AnswerCommentController::class)->group(function() {
-    Route::post('/answer-comments', 'store');
+    Route::post('/answer-comments', 'store')->name('answer-comments');
     Route::post('/answer-comments/{id}', 'update')->where('id', '[0-9]+')->name('edit-answer-comment');
     Route::post('/answer-comments/{id}/delete', 'destroy')->where('id', '[0-9]+')->name('delete-answer-comment');
 });
@@ -97,7 +97,7 @@ Route::post('/tags/edit', [TagController::class, 'update'])->name('edit-tag');
 Route::post('/tags/delete', [TagController::class, 'destroy'])->name('delete-tag');
 Route::post('/communities', [CommunityController::class, 'store'])->name('create-community');
 
-//Communities
+// Communities
 Route::get('/communities', [CommunityController::class, 'index'])->name('communities');
 Route::get('/communities/{id}', [QuestionController::class, 'communityIndex'])->where('id', '[0-9]+')->name('community');
 Route::get('/feed', [QuestionController::class, 'personalIndex'])->name('feed');
@@ -112,7 +112,6 @@ Route::post('api/questions/follow', [QuestionController::class, 'follow']);
 Route::post('api/questions/unfollow', [QuestionController::class, 'unfollow']);
 Route::post('api/tags/follow', [TagController::class, 'follow']);
 Route::post('api/tags/unfollow', [TagController::class, 'unfollow']);
-Route::post('api/questions/edit/remove-tag/{questionId}/{tagId}', [QuestionController::class, 'remove_tag']);
 Route::post('api/notifications/read', [NotificationController::class, 'read']);
 Route::post('api/questions/vote', [QuestionVoteController::class, 'vote']);
 Route::post('api/questions/unvote', [QuestionVoteController::class, 'unvote']);
@@ -121,7 +120,7 @@ Route::post('api/answers/unvote', [AnswerVoteController::class, 'unvote']);
 Route::post('api/answers/{id}/correct', [AnswerController::class, 'markCorrect']);
 Route::post('api/answers/{id}/incorrect', [AnswerController::class, 'markIncorrect']);
 
-// Static info pages
+// Static Pages
 Route::view('/about-contact-us', 'pages.about-contact-us')->name('about-contact-us');
 Route::view('/main-features', 'pages.main-features')->name('main-features');
 Route::view('/faq', 'pages.faq')->name('faq');
